761287fd72ad60affe0378dcb51cdbcf3455c154
jinwoo55 <jinwoo5555@naver.com>
2022-02-10 07:38:23 +0900

    Update bitcoinAutoTradeWithMA.py

--- pyupbit-autotrade-main/bitcoinAutoTradeWithMA.py
+++ pyupbit-autotrade-main/bitcoinAutoTradeWithMA.py
@@ -8,11 +8,12 @@ secret = "6Z3O9rECWdKRxIOs0NYQNwy1IogDfwWhTusxlWjd"
 myToken = "xoxb-3070117775266-3093895970272-7rceqOSe0zpRMlRiTVPgQCHr"
 
 def post_message(token, channel, text):
-    """슬랙 메시지 전송"""
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
         data={"channel": channel,"text": text}
     )
+    print(response)
+
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -45,44 +46,63 @@ def get_balance(ticker):
 
 def get_current_price(ticker):
     """현재가 조회"""
-    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+    return pyupbit.get_current_price(ticker=ticker)
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 # 시작 메세지 슬랙 전송
 post_message(myToken,"#crypto", "autotrade start")
 
+#돌릴 코인 명
+coin = ["KRW-BTC", "KRW-XRP", "KRW-ETH", "KRW-MATIC"]
+
+#현재 보유 현금 (코인 개수만큼 나눠서 투자)
+krw = get_balance("KRW")/len(coin)
+
+print("자동매매 시작")
+
 # 자동매매 시작
 while True:
     try:
 
-        #돌릴 코인 명
-        coin = ["KRW-BTC", "KRW-XRP", "KRW-ETH", "KRW-MATIC"]
-
-        #현재 보유 현금 (코인 개수만큼 나눠서 투자)
-        krw = get_balance("KRW")/len(coin)
+        print()
+        print("-----------------------")
+        oriKrw = get_balance("KRW")
+        print("잔여 현금가 : " + str(oriKrw))
+        print("구매 설정가 : " + str(krw))
+        
 
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
 
-        k = 0.5
+        k = 0.3
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             for selCoin in coin:
-                target_price = get_target_price(selCoin, k)
-                ma15 = get_ma15(selCoin)
-                current_price = get_current_price(selCoin)
-                if target_price < current_price and ma15 < current_price:
-                    if krw > 5000:
-                        buy_result = upbit.buy_market_order(selCoin, krw*0.9995)
-                        post_message(myToken,"#crypto",  selCoin + " buy : " +str(buy_result))
+                getCoinBal = get_balance(selCoin.replace('KRW-', ''))
+                if getCoinBal * get_current_price(selCoin) < 5000:
+                    print("진입 코인 " + selCoin)
+                    target_price = get_target_price(selCoin, k)
+                    ma15 = get_ma15(selCoin)
+                    current_price = get_current_price(selCoin)
+                    # print("목표가" + str(target_price))
+                    # print("현재가" + str(current_price))
+                    # print("이평선 " + str(ma15))
+                    if target_price < current_price and ma15 < current_price:
+                        if krw > 5000:
+                            buy_result = upbit.buy_market_order(selCoin, krw*0.9995)
+                            print(selCoin + " 코인 " + str(krw*0.9995) + " KRW 매수")
+                            post_message(myToken,"#crypto",  selCoin + " buy : " +str(buy_result))
+                    time.sleep(1)
         else:
             for selCoin in coin:
                 getCoinBal = get_balance(selCoin.replace('KRW-', ''))
                 if getCoinBal * get_current_price(selCoin) > 5000:
                     sell_result = upbit.sell_market_order(selCoin, getCoinBal*0.9995)
                     post_message(myToken,"#crypto", selCoin + "sell : " +str(sell_result))
+                    krw = get_balance("KRW")/len(coin)
+                    
         time.sleep(1)
     except Exception as e:
         print(e)
